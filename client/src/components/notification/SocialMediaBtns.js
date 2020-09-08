5449f386d9661cfa11b65054cdc67430e3b98a1d
Shinell Manwaring <shinell.manwaring@gmail.com>
2020-09-08 00:30:00 -0700

    Created social media buttons and added them to the confirmation page, which is shown after submitting your votes.

--- client/src/components/notification/SocialMediaBtns.js
+++ client/src/components/notification/SocialMediaBtns.js
@@ -6,19 +6,17 @@ import {
   RedditShareButton,
   TumblrShareButton,
   PinterestShareButton,
-  WhatsappShareButton,
   FacebookIcon,
   LinkedinIcon,
   TwitterIcon,
   RedditIcon,
   TumblrIcon,
-  PinterestIcon,
-  WhatsappIcon,
+  PinterestIcon
 } from "react-share";
        
 export default function SocialMediaButtons() {
     return (
-        <div id="social-media-btns-container">
+        <div className="social-media-btns-container">
             <LinkedinShareButton 
                 url={"https://the-shoppies-awards.herokuapp.com"}
                 title={"The Shoppies Awards  - Nominate your favorite movies"}
@@ -33,32 +31,27 @@ export default function SocialMediaButtons() {
             </FacebookShareButton>
             <TwitterShareButton 
                 url={"https://the-shoppies-awards.herokuapp.com"}
-                title={"The Shoppies - Nominate your favorite movies"}
+                title={"The Shoppies Awards - Nominate your favorite movies"}
                 hashtag="#theshoppiesawards">
                 <TwitterIcon size={36} />
             </TwitterShareButton>
             <RedditShareButton
                 url={"https://the-shoppies-awards.herokuapp.com"}
-                title={"The Shoppies - Nominate your favorite movies"}>
+                title={"The Shoppies Awards - Nominate your favorite movies"}>
                 <RedditIcon size={36} />
             </RedditShareButton>
             <TumblrShareButton
                 url={"https://the-shoppies-awards.herokuapp.com"}
-                title={"The Shoppies - Nominate your favorite movies"}
+                title={"The Shoppies Awards - Nominate your favorite movies"}
                 caption="">
                 <TumblrIcon size={36} />
             </TumblrShareButton>
             <PinterestShareButton
                 url={"https://the-shoppies-awards.herokuapp.com"}
-                description={"The Shoppies - Nominate your favorite movies"}>
+                media={"https://i.imgur.com/7rTrLC9.png"}
+                description={"The Shoppies Awards - Nominate your favorite movies"}>
                 <PinterestIcon size={36} />
             </PinterestShareButton>
-            <WhatsappShareButton 
-                url={"https://the-shoppies-awards.herokuapp.com"}
-                title={"The Shoppies - Nominate your favorite movies"}
-                separator=":: ">
-                <WhatsappIcon size={36} />
-            </WhatsappShareButton>
         </div>
     )
 }
