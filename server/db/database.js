71a8dd432faf39750313a51fcdcd08a5451c52e2
Shinell Manwaring <shinell.manwaring@gmail.com>
2021-05-21 13:00:33 -0700

    Updated the creation of my database variable. When an API request was previously made to the database, the folowing error was shown 'SequelizeConnectionError]: no pg_hba.conf entry for host [...redacted], user [...redacted], database [...redacted], SSL off.

--- server/db/database.js
+++ server/db/database.js
@@ -7,9 +7,14 @@ console.log(chalk.yellow("Opening database connection"));
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const database = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  {
-    logging: (...msg) => console.log(msg),
-  }
-)
+    process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+    {
+      logging: (...msg) => console.log(msg),
+      dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+      }
+    }
+  )
 module.exports = database;
